37c42c70f67e4351efc9bfef3b02cd322b2ddc3b
Egoitz <egoitzu@telefonica.net>
2022-09-24 23:12:38 +0200

    First session of my ToDoApp project
    This is my first session creating my ToDoApp project in react.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import './App.css';
+
+//Importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Ego's Todo List</h1>
+      </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+function Form({setInputText, todos, setTodos, inputText}) {
+    function inputTextHandler(e) {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    function submitTodoHandler(e){
+        e.preventDefault();
+        setTodos([
+           ...todos, {text: inputText, completed: false, id: Math.random() * 1000} 
+        ]);
+        setInputText("");
+    }
+    return(
+        <form>
+            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"></input>
+                <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                    <i className="fas fa-plus-square"></i>
+                </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>    
+        </form>
+    );
+}
+
+export default Form;
